3a327fee617235685f87289ba9e33e003072f280
Marcela F <54597344+marcelala@users.noreply.github.com>
2021-11-14 12:32:37 +0100

    touch up styles

--- src/components/CloudFile.tsx
+++ src/components/CloudFile.tsx
@@ -3,6 +3,8 @@ import iFile from "../types/iFile";
 import { FormEvent } from "react";
 import { deleteDocument } from "../firebaseServices/firestore";
 import { deleteFile } from "../firebaseServices/storage";
+import moment from "moment";
+import { formatBytes } from "../scripts/formatBytes";
 
 type iProps = {
   file: iFile;
@@ -11,6 +13,8 @@ type iProps = {
 export default function CloudFile({ file }: iProps) {
   const { name, fileURL, author, metadata, id } = file;
   const { size, timeCreated, fullPath, extension } = metadata;
+  const date = moment(timeCreated).format("DD-MM-YYYY");
+  const timeAgo = moment(timeCreated).startOf("hour").fromNow();
 
   async function onDelete(e: FormEvent) {
     e.preventDefault();
@@ -27,15 +31,20 @@ export default function CloudFile({ file }: iProps) {
     <li>
       <a href={fileURL} target="_blank" rel="noreferrer" download>
         <Icon fileName={extension} />
-        <span>{name}</span>
-        <span>{size}</span>
-        <span>{author}</span>
-        <span>{timeCreated}</span>
+        <span className="fileName">{name}</span>
       </a>
+      <span className="author">{author}</span>
+      <div className={"time"}>
+        <span>{date}</span>
+        <span>{timeAgo}</span>
+      </div>
+      <div className="size">
+        <span>{formatBytes(size)}</span>
 
-      <button onClick={(e) => onDelete(e)} className={"btn-delete"}>
-        <Icon fileName={"cloud-off"} />
-      </button>
+        <button onClick={(e) => onDelete(e)} className={"btn-delete"}>
+          <Icon fileName={"cloud-off"} />
+        </button>
+      </div>
     </li>
   );
 }
